249e4f2ce7160a3ee1c311f64b6010045096c2ea
Rimba Erlangga <rimbaerlangga@gmail.com>
2021-03-20 00:04:03 +0700

    fix

--- app.py
+++ app.py
@@ -6,12 +6,27 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+# load model
 file_name = "models/model.pkl"
 with open(file_name, 'rb') as f:
     model = pickle.load(f)
 
-@app.route('/main.html', methods=['GET', 'POST'])
+@app.route('/main.html', methods=['POST'])
 def predict():
+    '''
+    Predict the probability of the given news title.
+
+    Arguments
+    ---------
+        N/A
+
+    Returns
+    -------
+        a dict D such that `D['result'] = response` where
+        `response` is a string containing probability of
+        predicted class of the given news title
+
+    '''
     news_title = request.form['news_title']
     pct_sport = model.predict_proba([news_title])[0][1]
     if pct_sport >= 0.5:
@@ -29,4 +44,4 @@ def main():
         return render_template('main.html', predictions=predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- custom_objects.py
+++ custom_objects.py
@@ -1,6 +1,12 @@
 from sklearn.feature_extraction.text import TransformerMixin
 
 class DenseTransformer(TransformerMixin):
+    '''
+    Custom class inherited from sklearn TransformerMixin.
+    Used for transform the sparse matrix used in sklearn
+    TF-IDF vectorizer in the pipeline model.
+    '''
+    
     def fit(self, X, y=None, **fit_params):
         return self
 
